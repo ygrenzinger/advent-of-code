343df2bcd6477f7c22ee7e751e1d4de978c516c6
Yannick Grenzinger <yannick.grenzinger@gmail.com>
2021-12-06 17:36:39 +0100

    day 6

---
+++ src/Day06.kt
@@ -0,0 +1,55 @@
+import java.util.stream.Stream
+
+data class Lanternfish(val internalTimer: Int, val count: Long) {
+    fun dayPassing() : Stream<Lanternfish> {
+        val current = this.decreaseTimer()
+        return if (current.internalTimer >= 0) {
+            Stream.of(current)
+        } else {
+            Stream.of(Lanternfish(6,count), Lanternfish(8,count))
+        }
+    }
+
+    private fun decreaseTimer() = Lanternfish(internalTimer - 1, count)
+}
+
+fun List<Lanternfish>.dayPassing() : List<Lanternfish> {
+
+    val fishes = this.parallelStream()
+        .flatMap { it.dayPassing() }
+        .toList()
+    val newerCount = fishes.filter { it.internalTimer == 8 }.sumOf { it.count }
+    val dayPassed = if (newerCount > 0) {
+        fishes.filterNot { it.internalTimer == 8 } + Lanternfish(8, newerCount)
+    } else {
+        fishes
+    }
+    return dayPassed
+}
+
+fun main() {
+
+    fun part1(input: List<String>): Long {
+        val fishes = input.first().split(",")
+            .groupBy { it.toInt() }
+            .map { Lanternfish(it.key, it.value.size.toLong()) }
+        return (1..80).fold(fishes) { acc, _ -> acc.dayPassing() }.sumOf { it.count }
+    }
+
+    fun part2(input: List<String>): Long {
+        val fishes = input.first().split(",")
+            .groupBy { it.toInt() }
+            .map { Lanternfish(it.key, it.value.size.toLong()) }
+        return (1..256).fold(fishes) { acc, _ -> acc.dayPassing() }.sumOf { it.count }
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day06_test")
+    println(part1(testInput))
+    check(part2(testInput) == 26984457539L)
+
+    val input = readInput("Day06")
+    println(part1(input))
+    println(part2(input))
+}
+
