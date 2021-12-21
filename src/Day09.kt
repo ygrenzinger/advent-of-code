30697dbfc30fd8aa8b5e2794f2994d99a2290c7e
Yannick Grenzinger <yannick.grenzinger@gmail.com>
2021-12-21 18:44:52 +0100

    day 9 part 2

--- src/Day09.kt
+++ src/Day09.kt
@@ -1,54 +1,55 @@
+typealias Coordinate = Pair<Int, Int>
 
 fun main() {
 
-    fun buildMap(input: List<String>) : Map<Pair<Int, Int>, Int> {
-        return input.mapIndexed { i, line -> line.mapIndexed { j, column -> (i to j) to column.toString().toInt() }  }.flatten().toMap()
+    fun buildMap(input: List<String>): Map<Coordinate, Int> {
+        return input.mapIndexed { i, line -> line.mapIndexed { j, column -> (i to j) to column.toString().toInt() } }
+            .flatten().toMap()
     }
 
-    val adjacents = listOf(
+    val adjacentCells = listOf(
         (-1 to 0),
-        (0 to -1),(0 to 1),
+        (0 to -1), (0 to 1),
         (1 to 0)
     )
 
+    fun buildAdjacentCells(coord: Coordinate): List<Coordinate> {
+        return adjacentCells.map { adj -> (coord.first + adj.first) to (coord.second + adj.second) }
+    }
+
     fun part1(input: List<String>): Int {
         val map = buildMap(input)
-        val t: List<Int> = map.keys.map { coord ->
+        return map.keys.map { coord ->
             val currentHeight = map[coord]!!
-            val coordsToCheck : List<Pair<Int, Int>> = adjacents.map { adj -> (coord.first + adj.first) to (coord.second + adj.second) }
-            if (coordsToCheck.all {  currentHeight < map.getOrDefault(it, 9) }) {
+            val coordsToCheck: List<Coordinate> = buildAdjacentCells(coord)
+            if (coordsToCheck.all { currentHeight < map.getOrDefault(it, 9) }) {
                 currentHeight + 1
             } else {
                 0
             }
+        }.sum()
+    }
+
+    fun bassinSize(map: Map<Coordinate, Int>, visited: MutableMap<Coordinate, Boolean>, coord: Coordinate): Int {
+        return if (visited.getOrDefault(coord, true)) {
+            0
+        } else {
+            visited[coord] = true
+            buildAdjacentCells(coord).fold(1) { acc, c -> acc + bassinSize(map, visited, c) }
         }
-        return t.sum()
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        val map = buildMap(input)
+        val visited = map.keys.associateWith { (map.getOrDefault(it, 9) == 9) }.toMutableMap()
+        val bassinSizes: List<Int> = map.keys.fold<Coordinate, List<Int>>(listOf()) { acc, coord -> acc + bassinSize(map, visited, coord) }.filterNot { it == 0 }
+        return bassinSizes.sorted().reversed().take(3).reduce { a, b -> a * b }
     }
-
-    check(part1(listOf(
-        "0"
-    )) == 1)
-
-    check(part1(listOf(
-        "01",
-        "10"
-    )) == 2)
-
-
-    check(part1(listOf(
-        "020",
-        "212",
-        "020",
-    )) == 6)
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day09_test")
-    println(part1(testInput))
-    check(part1(testInput) == 15)
+    println(part2(testInput))
+    check(part2(testInput) == 1134)
 
     val input = readInput("Day09")
     println(part1(input))
